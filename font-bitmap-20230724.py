b3d7a982785af822bab41421b39da105fea43412
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-24 18:28:25 +0900

    Update font-bitmap-20230724.py

--- font-bitmap-20230724.py
+++ font-bitmap-20230724.py
@@ -1,16 +1,16 @@
-import subprocess
-subprocess.run(['pip','install','pillow'], capture_output=True)
+from PIL import Image, ImageDraw, ImageFont, ImageChops
 
-from PIL import Image, ImageDraw, ImageFont
+font = ImageFont.truetype('DungGeunMo.ttf', 16)
 
-font = ImageFont.truetype('neodgm.ttf', 12)
+def saveOnFile( im, nm ):
+  im.save(f'{nm}.png')
 
-def saveOnFile( ch ):
+def image( ch ):
   im = Image.new(mode='RGBA', size=(100,100))
   dr = ImageDraw.Draw(im, 'RGBA')
   dr.rectangle((0,0,100,100),fill=(255,255,255,0))
   dr.text((0,0), ch, (0,0,0), font=font)
-  im.save(f'{ch}.png')
+  return im
 
-ch = input('문자: ')
-saveOnFile(ch)
+def multi( im1, im2 ):
+  im3 = ImageChops.multiply(im1, im2)
