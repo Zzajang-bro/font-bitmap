a57c77479c4853f6ca40bcf16b6c47f53153690d
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-24 18:06:19 +0900

    Update font-bitmap-20230724.py

--- font-bitmap-20230724.py
+++ font-bitmap-20230724.py
@@ -6,7 +6,11 @@ from PIL import Image, ImageDraw, ImageFont
 font = ImageFont.truetype('neodgm.ttf', 12)
 
 def saveOnFile( ch ):
-  im = Image.new(mode="RGB", size=(100,100))
-  dr = ImageDraw.Draw(im)
+  im = Image.new(mode='RGBA', size=(100,100))
+  dr = ImageDraw.Draw(im, 'RGBA')
+  dr.rectangle((0,0,100,100),fill=(255,255,255,0))
   dr.text((0,0), ch, (0,0,0), font=font)
-  img.save(f'{ch}.png')
+  im.save(f'{ch}.png')
+
+ch = input('문자: ')
+saveOnFile(ch)
